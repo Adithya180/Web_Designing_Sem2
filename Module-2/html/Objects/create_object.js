f30cd246792af33c8b66f62215989e04d3aa6ab3
Adithya180 <adithyakottarathil186@gmail.com>
2023-08-23 09:37:28 +0530

    module 2
    all assignments and projects related to module 2

---
+++ Module-2/html/Objects/create_object.js
@@ -0,0 +1,6 @@
+const student = {
+    fname: "Sahail",
+    lname: "Dutta",
+    age: 19
+};
+console.log(student.fname, student.lname, student.age)
---
+++ Module-2/html/Objects/for_objects.js
@@ -0,0 +1,9 @@
+const student={
+    fname: "Sahail",
+    lname: "Dutta",
+    age: 19,
+    country: "India"
+};
+for(i in student){
+    console.log(student[i]);
+}
